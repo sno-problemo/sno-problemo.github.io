8bfeb277fa7d7a7f54d7497ced4e333bb5ee7a2a
sno-problemo <rhubbard117@gmail.com>
2024-10-02 12:29:38 -0400

    Update 3d-flight-simulator.js

--- 3d-flight-simulator.js
+++ 3d-flight-simulator.js
@@ -68,7 +68,7 @@ function loadPlaneModel() {
             // Adjust the loaded plane's orientation to match the environment
             //loadedPlane.rotation.x = Math.PI; // Rotate around X to face forward (-Z direction in Three.js)
             //loadedPlane.rotation.z = Math.PI / 2; // This rotation might be necessary depending on the original model orientation
-			loadedPlane.rotation.y = Math.PI; // Aligns the nose to face forward (-Z direction in Three.js)
+			loadedPlane.rotation.y = -Math.PI / 2; // Aligns the nose to face forward (-Z direction in Three.js)
 
 
             // Add the loaded plane to the wrapper
