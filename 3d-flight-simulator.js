db2f40a329187a6a8741ca1298941d8dbc324f00
sno-problemo <rhubbard117@gmail.com>
2024-10-02 07:03:29 -0400

    Update 3d-flight-simulator.js

--- 3d-flight-simulator.js
+++ 3d-flight-simulator.js
@@ -70,7 +70,8 @@ function loadPlaneModel() {
             planeWrapper.scale.set(1, 1, 1);
 
             // Rotate the wrapper to align the plane correctly
-            planeWrapper.rotation.y = Math.PI; // Adjust this value as needed
+            // planeWrapper.rotation.y = Math.PI; // Adjust this value as needed
+			planeWrapper.rotation.x = -Math.PI / 2;
 
             // Add an axes helper to visualize the orientation of the plane
             const axisHelper = new THREE.AxesHelper(5);
